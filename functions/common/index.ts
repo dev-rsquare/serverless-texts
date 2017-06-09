39ae11f5e185a25e4582cb9fd015c04b4b838118
bglee <deptno@gmail.com>
2017-06-09 17:13:25 +0900

    credential true

--- functions/common/index.ts
+++ functions/common/index.ts
@@ -1,7 +1,11 @@
 import {APIGatewayEvent} from 'aws-lambda';
 import {DynamoDB} from 'aws-sdk';
 
-const headers = {'Access-Control-Allow-Origin' : '*'};
+const headers = {
+    'Access-Control-Allow-Methods' : '*',
+    'Access-Control-Allow-Origin' : '*',
+    "Access-Control-Allow-Credentials" : true
+};
 
 export const dynamoDb = new DynamoDB.DocumentClient();
 export const isDev = (event: APIGatewayEvent) => event.requestContext.stage === 'dev';
